f856fd977078be0f0029a816d270e9b86ff3af80
Elian Acuña <eacunap@miumg.edu.gt>
2023-04-17 22:51:53 -0600

    ultima actualización sabado 17/04/2023

--- app/src/main/java/com/eacunap/firebaseapp/activities/RegisterActivity.java
+++ app/src/main/java/com/eacunap/firebaseapp/activities/RegisterActivity.java
@@ -2,20 +2,27 @@ package com.eacunap.firebaseapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.DatePicker;
 
 import com.eacunap.firebaseapp.databinding.ActivityRegisterBinding;
+
+import java.util.Calendar;
 
 public class RegisterActivity extends AppCompatActivity {
 
     //Binding
-    protected ActivityRegisterBinding binding;
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
 
     }
